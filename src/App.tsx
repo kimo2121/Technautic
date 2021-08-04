e37446e39a5bdec609a9518f34abedeb8b764577
Kareem Muhammad <67168549+kimo2121@users.noreply.github.com>
2021-08-04 16:20:36 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,12 @@ import CreateSingle from "./components/CreateSingle/CreateSinglePage";
 import Market from "./pages/MarketPage";
 import Profile from "./pages/ProfilePage";
 import TopSellers from "./components/TopSellers/TopSellersPage";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import Detail from "./components/Detail/DetailPage";
 import TopFlatters from "./components/TopFlatters/TopFlattersPage";
 // import OwlCarousel from "react-owl-carousel";
